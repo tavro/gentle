690f16d35818973a68d436e87a3ede7961b3fee1
Viktor Holta <viktor.holta@outlook.com>
2023-09-30 12:50:36 +0200

    NEW: add cmake and gitignore

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@
 #include <vector>
 #include <map>
 
-#include "./headers/editor/explorer.h"
-#include "./headers/editor/heirarchy.h"
-#include "./headers/editor/inspector.h"
+//#include "./headers/editor/explorer.h"
+//#include "./headers/editor/heirarchy.h"
+//#include "./headers/editor/inspector.h"
 
 #include "./headers/texture.h"
 #include "./headers/particle.h"
@@ -29,6 +29,7 @@
 #include "./headers/image.h"
 #include "./headers/ui_panel.h"
 #include "./headers/game_object.h"
+#include "./headers/scene.h"
 
 const int UI_AREA = 256+64;
 const int SCREEN_FPS = 60;
@@ -50,17 +51,11 @@ Image mainMenuImg{0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
 
 Text FPSText{"", 0, SCREEN_HEIGHT - 28};
 
-Explorer explorer{ "", 0, SCREEN_HEIGHT+32, SCREEN_WIDTH/2, UI_AREA };
-Heirarchy heirarchy{ SCREEN_WIDTH/2, SCREEN_HEIGHT+32, SCREEN_WIDTH/2, UI_AREA };
-Inspector inspector{SCREEN_WIDTH, 0};
+//Explorer explorer{ "", 0, SCREEN_HEIGHT+32, SCREEN_WIDTH/2, UI_AREA };
+//Heirarchy heirarchy{ SCREEN_WIDTH/2, SCREEN_HEIGHT+32, SCREEN_WIDTH/2, UI_AREA };
+//Inspector inspector{SCREEN_WIDTH, 0};
 
 Scene* testScene = new Scene{};
-
-Button saveButton{"Save", 0, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT};
-Button cdButton{"cd ..", BUTTON_WIDTH*2+16*2, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT};
-Button createButton{"Create Empty Obj", BUTTON_WIDTH*3+16*3, SCREEN_HEIGHT};
-
-InputField field{BUTTON_WIDTH+16,  SCREEN_HEIGHT, 96, 28, 8};
 
 GameObject a{};
 
@@ -128,43 +123,19 @@ bool loadMedia()
 	bool success = true;
 
 	testScene->load("./test.scene");
-	testScene->loadTextures(renderer);
 
-	heirarchy.loadTextures(renderer);
-	heirarchy.setActiveScene(testScene, renderer);
+	//heirarchy.loadTextures(renderer);
+	//heirarchy.setActiveScene(testScene, renderer);
 
-	inspector.setActiveObj(&a);
-	inspector.loadFont(renderer);
+	//inspector.setActiveObj(&a);
+	//inspector.loadFont(renderer);
 
-	explorer.folderTexture.loadFromFile( "./resources/folderSheet.png", renderer );
-	explorer.fileTexture.loadFromFile( "./resources/fileSheet.png", renderer );
-	explorer.texture.loadFromFile( "./resources/explorer.png", renderer );
+	//explorer.folderTexture.loadFromFile( "./resources/folderSheet.png", renderer );
+	//explorer.fileTexture.loadFromFile( "./resources/fileSheet.png", renderer );
+	//explorer.texture.loadFromFile( "./resources/explorer.png", renderer );
 
 	mainMenuImg.getTexture().loadFromFile( "./resources/main-menu-background.png", renderer );
 	canvas.addObj(&mainMenuImg);
-
-	saveButton.getText().loadFont( "./resources/font.ttf", 28 );
-	saveButton.loadSpriteSheet( "./resources/buttonsheet.png", renderer );
-	canvas.addObj(&saveButton);
-
-	createButton.getText().loadFont( "./resources/font.ttf", 28 );
-	createButton.loadSpriteSheet( "./resources/buttonsheet.png", renderer );
-	canvas.addObj(&createButton);
-
-	cdButton.getText().loadFont( "./resources/font.ttf", 28 );
-	cdButton.loadSpriteSheet( "./resources/buttonsheet.png", renderer );
-	canvas.addObj(&cdButton);
-
-	field.getText().loadFont( "./resources/font.ttf", 28 );
-	field.getTexture().loadFromFile( "./resources/buttonsheet.png", renderer );
-	for( int i = 0; i < 4; ++i )
-	{
-		field.getSpriteClip( i ).x = 0;
-		field.getSpriteClip( i ).y = i * BUTTON_HEIGHT;
-        field.getSpriteClip( i ).w = BUTTON_WIDTH;
-        field.getSpriteClip( i ).h = BUTTON_HEIGHT;
-	}
-	canvas.addObj(&field);
 
     success = !FPSText.loadFont( "./resources/font.ttf", 28 );
     success = !FPSText.loadTexture( renderer );
@@ -227,10 +198,7 @@ int main( int argc, char* args[] )
 			fpsTimer.start();
 
 			SDL_Color textColor = { 0, 0, 0, 0xFF };
-			
-			field.setText("sample.scene");
-			field.loadTextTexture(renderer);
-			
+
 			SDL_StartTextInput();
 
 			SDL_Rect camera = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
@@ -246,6 +214,7 @@ int main( int argc, char* args[] )
 					{
 						quit = true;
 					}
+					/*
 					else if( e.type == SDL_KEYDOWN )
 					{
 						// For heirarchy
@@ -260,57 +229,22 @@ int main( int argc, char* args[] )
 								inspector.setActiveObj(testScene->getObjFromName(heirarchy.getActiveObjName()));
 							break;
 						}
-
-						if( e.key.keysym.sym == SDLK_BACKSPACE)
-						{
-							field.removeChar();
-						}
-						else if( e.key.keysym.sym == SDLK_c && SDL_GetModState() & KMOD_CTRL )
-						{
-							SDL_SetClipboardText( field.getContent().c_str() );
-						}
-						else if( e.key.keysym.sym == SDLK_v && SDL_GetModState() & KMOD_CTRL )
-						{
-							field.updateText(SDL_GetClipboardText());
-						}
 					}
-					else if( e.type == SDL_TEXTINPUT )
-					{
-						if(!(SDL_GetModState() & KMOD_CTRL))
-						{
-							field.appendToText(e.text.text);
-						}
-					}
+					*/
 
 					canvas.handleEvent( &e );
-					explorer.handleEvent( &e );
+					//explorer.handleEvent( &e );
 
 				}
 
-				if(saveButton.isToggled())
-				{
-					testScene->save("");
-					saveButton.setToggle(false);
-				}
-				if(createButton.isToggled())
-				{
-					testScene->createEmptyObject();
-					heirarchy.setActiveScene(testScene, renderer);
-					createButton.setToggle(false);
-				}
-				if(cdButton.isToggled())
-				{
-					explorer.goBack();
-					cdButton.setToggle(false);
-				}
-
+				/*
 				if(explorer.fileHasChanged())
 				{
 					testScene->load(explorer.currentFile); // TODO: Check if scene
-					testScene->loadTextures(renderer);
 					heirarchy.setActiveScene(testScene, renderer);
 					explorer.toggleFileChanged();
 				}
+				*/
 
 				float avgFPS = countedFrames / ( fpsTimer.getTicks() / 1000.f );
 				if( avgFPS > 2000000 )
@@ -328,10 +262,9 @@ int main( int argc, char* args[] )
 
                 canvas.render(renderer);
 
-				testScene->render(renderer);
-				explorer.render(renderer);
-				heirarchy.render(renderer);
-				inspector.render(renderer);
+				//explorer.render(renderer);
+				//heirarchy.render(renderer);
+				//inspector.render(renderer);
 
 				SDL_RenderPresent( renderer );
 
